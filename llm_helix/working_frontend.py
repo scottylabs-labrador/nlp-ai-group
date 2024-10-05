aad5f15418266ad0895a69d316241845592728b2
MichaelHuynh13 <michaelhnanh@gmail.com>
2024-10-05 17:57:48 -0400

    Update working_frontend.py
    Starting it out :)

--- llm_helix/working_frontend.py
+++ llm_helix/working_frontend.py
@@ -1,2 +1,10 @@
 import streamlit as st
 
+
+
+message = st.chat_message("The Corpse of Andrew Carnegie",avatar="Andrew-Carnegie.jpg")
+message.write('Good day, what can I help you with in this beautiful day?')
+prompt = st.chat_input('Talk to Sir Carnegie!')
+
+if prompt:
+    st.write(f'the user has sent the follow prompt {prompt}')
