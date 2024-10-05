5030bd6d42d1e09d8bdde06b49ffca0a963e0c90
MichaelHuynh13 <michaelhnanh@gmail.com>
2024-10-05 18:12:04 -0400

    Update working_frontend.py

--- llm_helix/working_frontend.py
+++ llm_helix/working_frontend.py
@@ -1,8 +1,7 @@
 import streamlit as st
+import openai as ai
 
-
-
-message = st.chat_message("The Corpse of Andrew Carnegie",avatar="Andrew-Carnegie.jpg")
+message = st.chat_message("AC",avatar="Andrew-Carnegie.jpg")
 message.write('Good day, what can I help you with in this beautiful day?')
 prompt = st.chat_input('Talk to Sir Carnegie!')
 
